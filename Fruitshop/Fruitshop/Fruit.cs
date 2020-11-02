7b31453f8c4867d22dd7b3e7ed32f02f5f5a14f4
Kris Hermans <Kris.Hermans@live.be>
2020-11-02 18:43:10 +0100

    add Fruit

---
+++ Fruitshop/Fruitshop/Fruit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fruitshop
+{
+    public abstract class Fruit
+    {
+        public double Weight { get; set; }
+        public string Description { get; set; }
+    }
+}
